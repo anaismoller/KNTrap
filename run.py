e5c9d40765a88ffbf68fe2adb0047d2cde1d5feb
anaismoller <anaismoller@gmail.com>
2022-02-11 19:01:17 +1100

    ebug

--- run.py
+++ run.py
@@ -19,6 +19,9 @@ if __name__ == "__main__":
     parser.add_argument(
         "--path_out", type=str, default="./Fink_outputs", help="Path to outputs",
     )
+    parser.add_argument(
+        "--debug", action="store_true", help="Debug: one file processed only",
+    )
 
     args = parser.parse_args()
 
@@ -26,14 +29,16 @@ if __name__ == "__main__":
 
     cmd = (
         "python -W ignore 0_get_Fink_features_xmatch.py "
-        f"--path_field {args.path_field} --path_out {args.path_out} --run {args.run}"
+        f"--path_field {args.path_field} --path_out {args.path_out} --run {args.run} "
     )
-    # subprocess.check_call(shlex.split(cmd))
+    if args.debug:
+        cmd += "--debug"
+    subprocess.check_call(shlex.split(cmd))
 
     fname = args.path_field.split("/")[-1]
     print(f"Features from {fname}")
     cmd = (
         "python -W ignore 1_filter_top_candidates.py "
-        f"--fname {fname}_{args.run}.csv --path_out {args.path_out}"
+        f"--fname {fname}_{args.run}.csv --path_out {args.path_out} "
     )
     subprocess.check_call(shlex.split(cmd))
