a4b58d281d96db5a9a682288f4e4ebce27e04b8b
anaismoller <anaismoller@gmail.com>
2022-02-22 17:12:32 +1100

    no run number

--- run.py
+++ run.py
@@ -9,19 +9,26 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Wrapper for selection")
 
     parser.add_argument(
-        "--path_field", type=str, default="./S82sub8_tmpl", help="Path to field",
+        "--path_field",
+        type=str,
+        default="./S82sub8_tmpl",
+        help="Path to field",
     )
     parser.add_argument(
-        "--path_out", type=str, default="./Fink_outputs", help="Path to outputs",
+        "--path_out",
+        type=str,
+        default="./Fink_outputs",
+        help="Path to outputs",
     )
     parser.add_argument(
-        "--run", type=str, default="6", help="Run for ROBOT outputs",
+        "--debug",
+        action="store_true",
+        help="Debug: files processed in loop",
     )
     parser.add_argument(
-        "--debug", action="store_true", help="Debug: files processed in loop",
-    )
-    parser.add_argument(
-        "--test", action="store_true", help="Debug: one file processed only",
+        "--test",
+        action="store_true",
+        help="Debug: one file processed only",
     )
 
     args = parser.parse_args()
@@ -33,7 +40,7 @@ if __name__ == "__main__":
     # 0. process data
     cmd = (
         "python -W ignore 0_get_Fink_features_xmatch.py "
-        f"--path_field {path_field} --path_out {path_out} --run {args.run}"
+        f"--path_field {path_field} --path_out {path_out}"
     )
     if args.debug:
         cmd += "--debug"
