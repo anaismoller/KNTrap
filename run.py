77e7c1f68760f07d4426db14c7ca5642d7daf9e5
anaismoller <anaismoller@gmail.com>
2022-03-11 08:45:44 +1100

    pickle

--- run.py
+++ run.py
@@ -9,26 +9,16 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Wrapper for selection")
 
     parser.add_argument(
-        "--path_field",
-        type=str,
-        default="./S82sub8_tmpl",
-        help="Path to field",
+        "--path_field", type=str, default="./S82sub8_tmpl", help="Path to field",
     )
     parser.add_argument(
-        "--path_out",
-        type=str,
-        default="./Fink_outputs",
-        help="Path to outputs",
+        "--path_out", type=str, default="./Fink_outputs", help="Path to outputs",
     )
     parser.add_argument(
-        "--debug",
-        action="store_true",
-        help="Debug: files processed in loop",
+        "--debug", action="store_true", help="Debug: files processed in loop",
     )
     parser.add_argument(
-        "--test",
-        action="store_true",
-        help="Debug: one file processed only",
+        "--test", action="store_true", help="Debug: one file processed only",
     )
 
     args = parser.parse_args()
@@ -50,7 +40,7 @@ if __name__ == "__main__":
 
     # 1. Filtering
     tmp = Path(path_field).name
-    fname = f"{tmp}.csv"
+    fname = f"{tmp}.pickle"
     print(f"Features from {fname}")
     cmd = (
         "python -W ignore 1_filter_top_candidates.py "
